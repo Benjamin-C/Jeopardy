3551d805174af23e55070846a9197cb46d078a3c
705822 <705822@D-SOUT-166-11.athena.root>
2017-12-04 13:32:53 -0800

    Updating to Sortable form Int, need to fix 2 element list sorting

--- QuickSort/src/quickSort/QuickSort.java
+++ QuickSort/src/quickSort/QuickSort.java
@@ -5,53 +5,110 @@ import java.util.Random;
 
 public class QuickSort {
 	public static void main(String args[]) {
-		ArrayList<Integer> l = new ArrayList<Integer>();
+		ArrayList<Sortable> list = new ArrayList<Sortable>();
 		Random r = new Random();
 		int width = 16;
 		
 		for(int i = 0; i < 32; i++) {
-			l.add(r.nextInt(89) + 10);
+			int n = r.nextInt(89) + 10;
+			list.add(new Sortable(n, n));
 		}
 		
-		l = sort(l);
-		
-		System.out.print("<-- Begin List -->");
-		for(int i = 0; i < l.size(); i++) {
+		System.out.print("<-- Begin List -->" + list.size());
+		for(int i = 0; i < list.size(); i++) {
 			if(i % width == 0) {
 				System.out.println();
 			} else {
 				System.out.print(", ");
 			}
-			System.out.print(l.get(i));
+			System.out.print(list.get(i));
+		}
+		System.out.println();
+		System.out.println("<-- End List -->");
+		
+		list = sort(list);
+		
+		System.out.print("<-- Begin List -->" + list.size());
+		for(int i = 0; i < list.size(); i++) {
+			if(i % width == 0) {
+				System.out.println();
+			} else {
+				System.out.print(", ");
+			}
+			System.out.print(list.get(i));
 		}
 		System.out.println();
 		System.out.println("<-- End List -->");
 	}
+
+	public static ArrayList<Sortable> toSortable(ArrayList<Integer> o) {
+		ArrayList<Sortable> out = new ArrayList<Sortable>();
+		for(int i = 0; i < o.size(); i++) {
+			out.add(new Sortable(o.get(i), o.get(i)));
+		}
+		return out;
+	}
+	public static ArrayList<Integer> toInteger(ArrayList<Sortable> o) {
+		ArrayList<Integer> out = new ArrayList<Integer>();
+		for(int i = 0; i < o.size(); i++) {
+			out.add((Integer) o.get(i).getObject());
+		}
+		return out;
+	}
 	
 	@SuppressWarnings("unchecked")
-	public static ArrayList<Integer> sort(ArrayList<Integer> o) {
-		ArrayList<Integer> l = new ArrayList<Integer>();
-		ArrayList<Integer> h = new ArrayList<Integer>();
-		ArrayList<Integer> out = new ArrayList<Integer>();
+	public static ArrayList<Sortable> sort(ArrayList<Sortable> o) {
+		ArrayList<Sortable> l = new ArrayList<Sortable>();
+		ArrayList<Sortable> h = new ArrayList<Sortable>();
+		ArrayList<Sortable> out = new ArrayList<Sortable>();
 		
-		if(o.size() > 1) {
+		if(o.size() > 2) {
 			int pivot = o.size() / 2;
-			System.out.println(o.get(pivot));
 			for(int i = 0; i < o.size(); i++) {
-				if(o.get(i) < o.get(pivot)) {
-					l.add(o.get(i));
-				} else {
-					h.add(o.get(i));
+				if(i != pivot) {
+					if(o.get(i).getIndex() < o.get(pivot).getIndex()) {
+						l.add(o.get(i));
+					} else {
+						h.add(o.get(i));
+					}
 				}
 			}
-			sortp(l);
-			sortp(h);
-			//h = sort(h);
+
+			if(l.size() > 1) {
+				l = sort(l);
+			}
+			if(h.size() > 1) {
+				h = sort(h);
+			}
+			
+			out = (ArrayList<Sortable>) l.clone();
+			out.add(o.get(pivot));
+			out.addAll(h);
+			
+		} else {
+			if(o.size() == 2) {
+				int a = Math.min(o.get(0).getIndex(), o.get(1).getIndex());
+				int b = Math.max(o.get(0).getIndex(), o.get(1).getIndex());
+				out.add(new Sortable(a, a));
+				out.add(new Sortable(b, b));
+			} else {
+				out = (ArrayList<Sortable>) o.clone();
+			}
+			/*
+			if(o.size() == 2) {
+				if(o.get(0).getIndex() > o.get(1).getIndex()) {
+					out.add(o.get(1));
+					out.add(o.get(0));
+				} else {
+					if(o.get(0).getIndex() > o.get(1).getIndex()) {
+						out.add(o.get(1));
+						out.add(o.get(0));
+					}
+				}
+			} else {
+				out = (ArrayList<Sortable>) o.clone();
+			}*/
 		}
-		
-		out = l;
-		out.addAll(h);
-		
 		return out;
 	}
 	
@@ -59,20 +116,23 @@ public class QuickSort {
 	public static ArrayList<Integer> sortp(ArrayList<Integer> o) {
 		ArrayList<Integer> l = new ArrayList<Integer>();
 		ArrayList<Integer> h = new ArrayList<Integer>();
-		ArrayList<Integer> out = (ArrayList<Integer>) o.clone();
+		ArrayList<Integer> out = new ArrayList<Integer>();
 		
-		if(o.size() > 1) {
+		if(o.size() > 2) {
 			int pivot = o.size() / 2;
-			System.out.println("p" + o.get(pivot));
+			System.out.println("piv: " + o.get(pivot));
 			for(int i = 0; i < o.size(); i++) {
-				if(o.get(i) < o.get(pivot)) {
-					l.add(o.get(i));
-				} else {
-					h.add(o.get(i));
+				if(i != pivot) {
+					if(o.get(i) < o.get(pivot)) {
+						l.add(o.get(i));
+					} else {
+						h.add(o.get(i));
+					}
 				}
 			}
-			System.out.println(l);
-			System.out.println(h);
+			
+			System.out.println("low: " + l);
+			System.out.println("hig: " + h);
 			if(l.size() > 1) {
 				l = sortp(l);
 			}
@@ -80,10 +140,21 @@ public class QuickSort {
 				h = sortp(h);
 			}
 			
+			System.out.println("lst: " + l);
+			System.out.println("hst: " + h);
 			out = (ArrayList<Integer>) l.clone();
+			out.add(o.get(pivot));
 			out.addAll(h);
+			
+		} else {
+			if(o.size() == 2) {
+				out.add(Math.min(o.get(0), o.get(1)));
+				out.add(Math.max(o.get(0), o.get(1)));
+			} else {
+				out = (ArrayList<Integer>) o.clone();
+			}
 		}
-
+		System.out.println("ret: " + out);
 		return out;
 	}
 }
---
+++ QuickSort/src/quickSort/Sortable.java
@@ -0,0 +1,26 @@
+package quickSort;
+
+public class Sortable {
+	private Object o;
+	private int i;
+	
+	public Sortable(Object object, int index) {
+		super();
+		o = object;
+		i = index;
+	}
+	
+	public Object getObject() {
+		return o;
+	}
+	public void setObject(Object object) {
+		o = object;
+	}
+	public int getIndex() {
+		return i;
+	}
+	public void setIndex(int index) {
+		i = index;
+	}
+	
+}
