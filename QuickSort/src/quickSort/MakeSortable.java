51d1b5251046b9f1e9c7d2f35772ac631121196a
705822 <705822@D-SOUT-166-11.athena.root>
2017-12-15 11:51:50 -0800

    Added string sorting and made index double

---
+++ QuickSort/src/quickSort/MakeSortable.java
@@ -0,0 +1,23 @@
+package quickSort;
+
+import java.util.ArrayList;
+
+public class MakeSortable {
+	public static ArrayList<Sortable> makeStringSortable(ArrayList<String> s) {
+		int max = 0;
+		ArrayList<Sortable> out = new ArrayList<Sortable>();
+		for(int i = 0; i < s.size(); i++) {
+			max = Math.max(s.get(i).length(), max);
+		}
+		double loc = 0;
+		for(int i = 0; i < s.size(); i++) {
+			loc = 0;
+			for(int j = 0; j < s.get(i).length(); j++) {
+				char c = s.get(i).charAt(j);
+				loc = loc + (c * Math.pow(10, ((max - j - 1) * 2)));
+			}
+			out.add(new Sortable(s.get(i), loc));
+		}
+		return out;
+	}
+}
--- QuickSort/src/quickSort/Sortable.java
+++ QuickSort/src/quickSort/Sortable.java
@@ -2,9 +2,9 @@ package quickSort;
 
 public class Sortable {
 	private Object o;
-	private int i;
+	private double i;
 	
-	public Sortable(Object object, int index) {
+	public Sortable(Object object, double index) {
 		super();
 		o = object;
 		i = index;
@@ -16,7 +16,7 @@ public class Sortable {
 	public void setObject(Object object) {
 		o = object;
 	}
-	public int getIndex() {
+	public double getIndex() {
 		return i;
 	}
 	public void setIndex(int index) {
