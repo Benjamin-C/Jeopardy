9c909434c2f06fe9371dbef5a5c3b71ae018f0be
705822 <705822@D-SOUT-166-09.athena.root>
2018-04-23 10:45:20 -0700

    Implementing networking

--- src/jeopardy/Main.java
+++ src/jeopardy/Main.java
@@ -1,16 +1,65 @@
 package jeopardy;
 
 import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	
 	static String mode = "KEYBOARD";
+	volatile static Serial ser;
 	
 	public static void main(String args[]) {
+		networkTest0();
+	}
+	
+	public static void networkTest0() {
+		System.out.println("Starting");
+		try {
+			Socket cientConn = new ServerSocket(8878).accept();
+			InputStream is = cientConn.getInputStream();
+			for(int i = 0; i < 16; i++) {
+				System.out.println(is.read());
+			}
+			cientConn.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void networkTest() {
+		System.out.println("Starting");
+		try {
+			byte[] address = {(byte)10,  (byte)197, (byte)131, (byte)229};
+			int port = 555;
+			Socket cientConn = new Socket(InetAddress.getLocalHost(), 8878);
+			InputStream is = cientConn.getInputStream();
+			for(int i = 0; i < 16; i++) {
+				System.out.println(is.read());
+			}
+			cientConn.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void serialTest() {
+		Scanner sca = new Scanner(System.in);
 		
-		Serial s = new Serial("", 0, new SerialEvent() {
+		List<String> ports = Serial.getPorts();
+		for(String st : ports) {
+			System.out.println(st);
+		}
+		System.out.println("Please type the name of the port you want");
+		
+		ser = new Serial(sca.nextLine(), 115200, new SerialEvent() {
 
 			@Override
 			public void onDataAvaliable(byte data) {
@@ -25,33 +74,27 @@ public class Main {
 			}
 			
 		});
-		s.startListening();
-		System.out.println("Starting");
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		ser.startListening();
 		
-		s.stopListening();
-		System.out.println("Stopping");
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		System.out.println("Ready");
 		
-		System.out.println("listening: " + s.checkListening());
-		
-		System.out.println("Done");
-		System.exit(0);
-		Game game = new Game(InputMode.ARDUINO);
-		game.begin();
-		
+		Thread t = new Thread() {
+			@Override
+			public void run() {
+				Scanner s = new Scanner(System.in);
+				while(true) {
+					ser.println(s.nextLine());
+				}
+			}
+		};
+
+		while(true) {}
 	}
 	
+	public void run() {
+		Game game = new Game(InputMode.ARDUINO);
+		game.begin();
+	}
 	
 	@SuppressWarnings("unused")
 	private void test() {
